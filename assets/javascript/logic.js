62ab18d3363ee783147af360bf92866d29b5ab60
Som Ramnani <som.ramnani@gmail.com>
2019-04-17 19:41:01 -0500

    more animations and pictures of skills

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,25 @@
 $(function() {
   var header = $(".navbar");
   var aboutme = $("#aboutmesection");
+  var projectsSection = $("#projectssection")
   var navItem = $(".nav-link");
 
   $(window).scroll(function() {    
       var scroll = $(window).scrollTop();
   
       if (scroll >= 602.4000244140625) {
-          header.addClass("fixed-top");
+          header.addClass("fixed-top fadeInDown");
           header.removeClass("bg-transparent");
-          header.addClass("fadeInDown")
+        
+          aboutme.addClass("aboutmeanimation slideInLeft")
+         
           
       } else {
           header.removeClass("fixed-top  fadeInDown");
           header.addClass("bg-transparent");
+
+          aboutme.removeClass("aboutmeanimation slideInLeft");
+         
       }
   });
 
